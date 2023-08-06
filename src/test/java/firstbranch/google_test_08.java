61f1bd0b0d194675ba1a9a61aa586490473a9dd7
Valerii <valera-asratyan90@yandex.ru>
2023-08-06 16:51:07 +0300

    change google test

---
+++ src/test/java/firstbranch/google_test_08.java
@@ -0,0 +1,15 @@
+package firstbranch;
+
+
+import org.junit.jupiter.api.Test;
+
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class google_test_08 {
+    @Test
+    void googleTest(){
+        open("https:google.com/");
+    }
+}
