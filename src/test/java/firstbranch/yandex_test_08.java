65799da1dc9e7edc4ecfa080812764062d8627da
Valera2023 <140104958+Valera2023@users.noreply.github.com>
2023-08-06 18:42:15 +0300

    Update yandex_test_08.java

--- src/test/java/firstbranch/yandex_test_08.java
+++ src/test/java/firstbranch/yandex_test_08.java
@@ -1,12 +1,1 @@
-package firstbranch;
 
-
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.open;
-
-public class yandex_test_08 {
-    @Test
-    void yandexTest(){
-    }
-}
